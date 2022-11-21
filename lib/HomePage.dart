9bef966ccae9ba93a0f750b701b704b1927c8ff6
Jason Guo <jasonyguo@gmail.com>
2022-11-21 17:23:25 -0500

    Basic routing

---
+++ lib/HomePage.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      constraints: const BoxConstraints.expand(),
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/naturebackground.jpg"),
+              fit: BoxFit.cover,
+              opacity: 0.35)),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: Text(
+                'PalliNet',
+                style: Theme.of(context).textTheme.headline2,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 80.0),
+              child: Text(
+                'Care for living',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                debugPrint("PushNamed");
+                Navigator.pushNamed(context, "/temp");
+                debugPrint("Start");
+              },
+              child: const Text("Button"),
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:pallinet/PatientLogin.dart';
+import 'HomePage.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const PalliNet());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class PalliNet extends StatelessWidget {
+  const PalliNet({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-      constraints: const BoxConstraints.expand(),
-      decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("assets/images/naturebackground.jpg"),
-              fit: BoxFit.cover,
-              opacity: 0.35)),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(bottom: 20.0),
-              child: Text(
-                'PalliNet',
-                style: Theme.of(context).textTheme.headline2,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 80.0),
-              child: Text(
-                'Care for living',
-                style: Theme.of(context).textTheme.headline5,
-              ),
-            ),
-            OutlinedButton(
-              onPressed: () {
-                debugPrint("Start");
-              },
-              child: const Text("Button"),
-            )
-          ],
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
         ),
-      ),
-    ));
+        // home: const MyHomePage(),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const HomePage(),
+          '/temp': (context) => const PatientLogin(),
+        });
   }
 }
