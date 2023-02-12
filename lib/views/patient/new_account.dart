390592f83f109741c2ce3b24709dc6de1b261d89
Jason Guo <jasonyguo@gmail.com>
2023-02-12 18:35:14 -0500

    Vaidation

--- lib/views/patient/new_account.dart
+++ lib/views/patient/new_account.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pallinet/constants.dart';
 
 class NewAccountPage extends StatefulWidget {
@@ -9,7 +13,7 @@ class NewAccountPage extends StatefulWidget {
 }
 
 class _NewAccountState extends State<NewAccountPage> {
-  final List<GlobalKey<FormState>> _formKeys = [GlobalKey<FormState>(), GlobalKey<FormState>()];
+  final List<GlobalKey<FormState>> _formKeys = [GlobalKey<FormState>(), GlobalKey<FormState>(), GlobalKey<FormState>()];
 
   bool isPasswordVissible = false;
 
@@ -39,6 +43,7 @@ class _NewAccountState extends State<NewAccountPage> {
               onStepContinue: () {
                 bool isLastStep = (currentStep == getSteps().length - 1);
                 if (isLastStep) {
+                  debugPrint("OwO");
                   //Do something with this information
                 } else {
                   setState(() {
@@ -119,63 +124,285 @@ class _NewAccountState extends State<NewAccountPage> {
             ),
           )),
       Step(
-        state: currentStep > 1 ? StepState.complete : StepState.indexed,
-        isActive: currentStep >= 1,
-        title: const Text("Patient Details"),
-        content: Column(
-          children: [
-            gap(),
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'First Name',
-                hintText: 'Enter your first name',
-                border: OutlineInputBorder(),
-              ),
+          state: currentStep > 1 ? StepState.complete : StepState.indexed,
+          isActive: currentStep >= 1,
+          title: const Text("Patient Details"),
+          content: Form(
+            key: _formKeys[1],
+            child: Column(
+              children: [
+                gap(),
+                TextFormField(
+                  validator: (value) => requiredValue(value),
+                  decoration: const InputDecoration(
+                    labelText: 'First Name',
+                    hintText: 'Enter your first name',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                gap(),
+                TextFormField(
+                  validator: (value) => requiredValue(value),
+                  decoration: const InputDecoration(
+                    labelText: 'Last Name',
+                    hintText: 'Enter your last name',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                gap(),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Expanded(
+                        flex: 3,
+                        child: DropdownButtonFormField(
+                          validator: (value) => requiredValue(value),
+                          items: Gender.values.map((e) {
+                            return DropdownMenuItem<Gender>(
+                              value: e,
+                              child: Text(e.value),
+                            );
+                          }).toList(),
+                          hint: const Text("Gender"),
+                          decoration: const InputDecoration(
+                            labelText: 'Gender',
+                          ),
+                          onChanged: (value) => {},
+                        )),
+                    const Expanded(
+                      flex: 1,
+                      child: SizedBox(),
+                    ),
+                    Expanded(
+                        flex: 5,
+                        child: TextFormField(
+                          validator: (value) => requiredValue(value),
+                          decoration: const InputDecoration(
+                            labelText: 'Birthdate',
+                            hintText: 'Enter your birthdate',
+                            // border: OutlineInputBorder(),
+                          ),
+                          inputFormatters: [DateTextFormatter()],
+                          keyboardType: const TextInputType.numberWithOptions(),
+                        )),
+                  ],
+                )
+              ],
             ),
-            gap(),
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Last Name',
-                hintText: 'Enter your last names',
-                border: OutlineInputBorder(),
-              ),
+          )),
+      Step(
+          state: currentStep > 2 ? StepState.complete : StepState.indexed,
+          isActive: currentStep >= 2,
+          title: const Text("Additional Contact Information"),
+          content: Form(
+            key: _formKeys[2],
+            child: Column(
+              children: [
+                gap(),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: DropdownButtonFormField(
+                        items: NumberType.values.map((e) {
+                          return DropdownMenuItem<NumberType>(
+                            value: e,
+                            child: Text(e.value),
+                          );
+                        }).toList(),
+                        hint: const Text("Type"),
+                        onChanged: (value) => {},
+                      ),
+                    ),
+                    const Expanded(
+                      flex: 1,
+                      child: SizedBox(),
+                    ),
+                    Expanded(
+                        flex: 5,
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: 'Phone Number',
+                            hintText: 'xxx-xxx-xxxx',
+                          ),
+                          inputFormatters: [PhoneNumberFormatter()],
+                          keyboardType: const TextInputType.numberWithOptions(),
+                        )),
+                    // TODO ability to add multiple contactss
+                  ],
+                )
+              ],
             ),
-          ],
-        ),
-      ),
+          )),
     ];
   }
 }
 
 emailValidation(value) {
-  if (value == null || value.isEmpty) {
-    return 'Please enter some text';
-  }
-  bool emailValid = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value);
-  if (!emailValid) {
-    return 'Please enter a valid email';
-  }
+  // if (value == null || value.isEmpty) {
+  //   return 'Please enter some text';
+  // }
+  // bool emailValid = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value);
+  // if (!emailValid) {
+  //   return 'Please enter a valid email';
+  // }
   return null;
 }
 
 passwordValidation(value) {
-  if (value == null || value.isEmpty) {
-    return 'Please enter some text';
-  }
-  if (value.length < 6) {
-    return 'Password must be at least 6 characters';
-  }
+  // if (value == null || value.isEmpty) {
+  //   return 'Please enter some text';
+  // }
+  // if (value.length < 6) {
+  //   return 'Password must be at least 6 characters';
+  // }
   return null;
 }
 
 passwordVerification(value, first) {
-  if (value == null || value.isEmpty) {
-    return 'Please enter some text';
-  }
-  debugPrint(value);
-  debugPrint(first);
-  if (value != first) {
-    return "Password does not match";
+  // if (value == null || value.isEmpty) {
+  //   return 'Please enter some text';
+  // }
+  // if (value != first) {
+  //   return "Password does not match";
+  // }
+  return null;
+}
+
+requiredValue(value) {
+  if (value.runtimeType == Gender) {
+    return null;
+  } else if (value.runtimeType == null || value.isEmpty) {
+    return 'Required field';
   }
   return null;
+}
+
+class DateTextFormatter extends TextInputFormatter {
+  static const _maxChars = 8;
+
+  @override
+  TextEditingValue formatEditUpdate(
+    TextEditingValue oldValue,
+    TextEditingValue newValue,
+  ) {
+    String separator = '/';
+    var text = _format(
+      newValue.text,
+      oldValue.text,
+      separator,
+    );
+
+    return newValue.copyWith(
+      text: text,
+      selection: updateCursorPosition(
+        oldValue,
+        text,
+      ),
+    );
+  }
+
+  String _format(
+    String value,
+    String oldValue,
+    String separator,
+  ) {
+    var isErasing = value.length < oldValue.length;
+    var isComplete = value.length > _maxChars + 2;
+
+    if (!isErasing && isComplete) {
+      return oldValue;
+    }
+
+    value = value.replaceAll(separator, '');
+    final result = <String>[];
+
+    for (int i = 0; i < min(value.length, _maxChars); i++) {
+      result.add(value[i]);
+      if ((i == 1 || i == 3) && i != value.length - 1) {
+        result.add(separator);
+      }
+    }
+
+    return result.join();
+  }
+
+  TextSelection updateCursorPosition(
+    TextEditingValue oldValue,
+    String text,
+  ) {
+    var endOffset = max(
+      oldValue.text.length - oldValue.selection.end,
+      0,
+    );
+
+    var selectionEnd = text.length - endOffset;
+
+    return TextSelection.fromPosition(TextPosition(offset: selectionEnd));
+  }
+}
+
+class PhoneNumberFormatter extends TextInputFormatter {
+  static const _maxChars = 10;
+
+  @override
+  TextEditingValue formatEditUpdate(
+    TextEditingValue oldValue,
+    TextEditingValue newValue,
+  ) {
+    String separator = '-';
+    var text = _format(
+      newValue.text,
+      oldValue.text,
+      separator,
+    );
+
+    return newValue.copyWith(
+      text: text,
+      selection: updateCursorPosition(
+        oldValue,
+        text,
+      ),
+    );
+  }
+
+  String _format(
+    String value,
+    String oldValue,
+    String separator,
+  ) {
+    var isErasing = value.length < oldValue.length;
+    var isComplete = value.length > _maxChars + 2;
+
+    if (!isErasing && isComplete) {
+      return oldValue;
+    }
+
+    value = value.replaceAll(separator, '');
+    final result = <String>[];
+
+    for (int i = 0; i < min(value.length, _maxChars); i++) {
+      result.add(value[i]);
+      if ((i == 2 || i == 5) && i != value.length - 1) {
+        result.add(separator);
+      }
+    }
+
+    return result.join();
+  }
+
+  TextSelection updateCursorPosition(
+    TextEditingValue oldValue,
+    String text,
+  ) {
+    var endOffset = max(
+      oldValue.text.length - oldValue.selection.end,
+      0,
+    );
+
+    var selectionEnd = text.length - endOffset;
+
+    return TextSelection.fromPosition(TextPosition(offset: selectionEnd));
+  }
 }
