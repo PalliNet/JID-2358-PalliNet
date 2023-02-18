cde6dfa087c3ff18f51a3707d0465454d75f92d7
jlin474 <jlin474@gatech.edu>
2023-02-18 13:19:48 -0500

    Created End-of-Life page UI
    Created the End of Life page UI utilizing methods to query and update information on the database.

--- lib/views/patient/patient_end_of_life_plans.dart
+++ lib/views/patient/patient_end_of_life_plans.dart
@@ -9,7 +9,90 @@ class EndOfLifePlansView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('End of Life Plans')),
-    );
+        appBar: AppBar(title: const Text('End of Life Plans')),
+        body:
+            const Padding(padding: EdgeInsets.all(16.0), child: PlanContent()));
+    // );
   }
+}
+
+class PlanContent extends StatefulWidget {
+  const PlanContent({Key? key}) : super(key: key);
+
+  @override
+  State<PlanContent> createState() => PlanContentState();
+}
+
+class PlanContentState extends State<PlanContent> {
+  bool isPasswordVisible = false;
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String? desc = "gkyhuiadfsss";
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Patient>(
+        future: retrievePatientProfile2(),
+        builder: ((context, snapshot) {
+          if (snapshot.hasData) {
+            Patient? patData = snapshot.data;
+            desc = patData!.description;
+            // debugPrint("4");
+            // debugPrint(desc);
+            return Container(
+              constraints: const BoxConstraints(maxWidth: 1000),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    gap(),
+                    gap(),
+                    const Text(
+                      'Update End of Life Plans',
+                      textAlign: TextAlign.left,
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                    gap(),
+                    TextFormField(
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                      minLines: 3,
+                      initialValue: desc,
+                      onSaved: (value) => {desc = value},
+                      decoration: const InputDecoration(
+                        hintText: 'Enter here',
+                        prefixIcon: Icon(Icons.description),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12)),
+                        ),
+                      ),
+                    ),
+                    gap(),
+                    ElevatedButton(
+                        onPressed: () {
+                          _formKey.currentState?.save();
+                          Map<String, dynamic> payload = {
+                            "description": desc,
+                          };
+                          // debugPrint("3");
+                          // debugPrint(desc);
+                          updateEndOfLifePlans(payload);
+                          Navigator.pushNamed(context, "/patient/home");
+                        },
+                        child: const Text("Change End of Life Plans"))
+                  ],
+                ),
+              ),
+            );
+          } else {
+            return const LoadingScreen("Loading End of Life Plans");
+          }
+        }));
+  }
+
+  Widget gap() => const SizedBox(height: 16);
 }
