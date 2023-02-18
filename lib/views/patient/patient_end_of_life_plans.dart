decd39dab211c1808827bba6094105a32de77769
jlin474 <jlin474@gatech.edu>
2023-02-18 13:13:19 -0500

    Created Methods for Querying from the database and Updating the database
    I created two methods for both retrieving and updating the end of life plans on the database and created a name model class for both patient model and physician model.

--- lib/views/patient/patient_end_of_life_plans.dart
+++ lib/views/patient/patient_end_of_life_plans.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pallinet/components/loading.dart';
+import 'package:pallinet/firestore/firestore.dart';
+import 'package:pallinet/models/patient_model.dart';
 
 class EndOfLifePlansView extends StatelessWidget {
   const EndOfLifePlansView({super.key});
