7865f2b99e8ec6e31ed901c2b64105760c98f76f
jlei46 <91035006+jlei46@users.noreply.github.com>
2023-03-12 15:24:43 -0400

    PAL-68 (#49)
    * PAL-68
    
    * Add popupmenubutton
    
    ---------
    
    Co-authored-by: Jason Guo <jasonyguo@gmail.com>

--- lib/views/physician/physician_appointments.dart
+++ lib/views/physician/physician_appointments.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:pallinet/components/appointment_card.dart';
 import 'package:pallinet/firestore/firestore.dart';
 import 'package:pallinet/models/session_manager.dart';
+
 class PhysicianAppointments extends StatefulWidget {
   const PhysicianAppointments({super.key});
 
@@ -26,39 +27,44 @@ class _PhysicianAppointmentsState extends State<PhysicianAppointments> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-
-        Expanded(flex: 8, child: FutureBuilder<List<dynamic>>(
-        future: _prefs.getUid().then((uid) => retrieveAppointmentsPhysicians(uid)),
-        builder: ((context, snapshot) {
-          final list = snapshot.data == null
-              ? []
-              : (snapshot.data as List)
-                  .map((e) => e as Map<String, dynamic>)
-                  .toList();
-          return Scaffold(
-            appBar: AppBar(title: const Text("Appointments")),
-            body: ListView.builder(
-              itemCount: list.length,
-              itemBuilder: (BuildContext context, int index) {
-                final data = list[index];
-                Timestamp t = data["scheduledTimeStart"] as Timestamp;
-                DateTime startTime = t.toDate();
-                return AppointmentCard(
-                  name: data["patient"],
-                  date: startTime,
-                  appointmentType: data["appointmentType"],
-                  id: data["appointmentID"],
-                );
-              },
-            ),
-          );
-         }),
+    return Column(children: [
+      Expanded(
+        flex: 8,
+        child: FutureBuilder<List<dynamic>>(
+          future: _prefs
+              .getUid()
+              .then((uid) => retrieveAppointmentsPhysicians(uid)),
+          builder: ((context, snapshot) {
+            final list = snapshot.data == null
+                ? []
+                : (snapshot.data as List)
+                    .map((e) => e as Map<String, dynamic>)
+                    .toList();
+            return Scaffold(
+              appBar: AppBar(title: const Text("Appointments")),
+              body: ListView.builder(
+                itemCount: list.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final data = list[index];
+                  Timestamp t = data["scheduledTimeStart"] as Timestamp;
+                  DateTime startTime = t.toDate();
+                  return AppointmentCard(
+                    name: data["patient"],
+                    date: startTime,
+                    appointmentType: data["appointmentType"],
+                    id: data["appointmentID"],
+                  );
+                },
+              ),
+            );
+          }),
         ),
       ),
-      Expanded(flex: 2, child: ElevatedButton(
-            onPressed: () => {Navigator.pushNamed(context, "/physician/appointments/new")},
+      Expanded(
+        flex: 2,
+        child: ElevatedButton(
+            onPressed: () =>
+                {Navigator.pushNamed(context, "/physician/appointments/new")},
             style: ElevatedButton.styleFrom(
               backgroundColor: Colors.white, // Background color
             ),
@@ -77,8 +83,8 @@ class _PhysicianAppointmentsState extends State<PhysicianAppointments> {
                   style: TextStyle(fontSize: 20, color: Colors.black),
                 )
               ],
-            )),)
-      ]
-    );
+            )),
+      )
+    ]);
   }
 }
